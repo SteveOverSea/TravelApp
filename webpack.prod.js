6e2d92f7ef4241e174adaedffd4ff40cf20b10ed
Stefan Rausch <rausch.stefan@edumail.at>
2021-04-25 12:46:46 +0200

    sass config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,24 @@
 const Path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "production",
     entry: "./src/client/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "main.css"
         })
     ],
     output: {
