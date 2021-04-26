e72eb78e91ecc8e7a31c49521685efcd885537c2
Stefan Rausch <rausch.stefan@edumail.at>
2021-04-26 09:25:11 +0200

    add geodata & weatherbit API calls

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,63 @@
-console.log("app.js");
+import { zip } from "lodash";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("submit").addEventListener("click", apiRequestHandler);
+});
+
+async function apiRequestHandler (event) {
+    event.preventDefault();
+
+    const zipCode = document.getElementById("zip").value;
+    const countryCode = document.getElementById("country").value;
+
+    const placesDiv = document.getElementById("possible-places");
+    placesDiv.innerHTML = "";
+
+    try {
+       const response = await fetch("/submit", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ zipCode, countryCode })
+        });
+
+        const responseData = await response.json();
+
+        const select = document.createElement("select");
+        const selectButton = document.createElement("button");
+        selectButton.innerHTML = "select";
+
+        responseData.forEach(el => {
+            const option = document.createElement("option");
+            option.value = el.lat + "," + el.lng;
+            option.innerHTML = el.placeName;
+            select.appendChild(option);
+        });
+
+        initSendGeoData(selectButton, select);
+
+        placesDiv.appendChild(select);
+        placesDiv.appendChild(selectButton);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function initSendGeoData (button, select) {
+    const value = select.value;
+    const lat = value.split(",")[0];
+    const lng = value.split(",")[1];
+
+    button.addEventListener("click", async () => {
+        
+        fetch ("/geodata", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ lat, lng })
+        });
+    })
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,57 @@
 const express = require("express");
 const app = express();
 
+const dotenv = require("dotenv");
+dotenv.config();
+const fetch = require("node-fetch");
+
 const PORT = 8080;
 
 app.use(express.static("dist"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(PORT, () => console.log("listening on port " + PORT));
+
+app.post("/submit", async (req, res) => {
+
+    geonamesResponse = await getGeonamesData(req.body.zipCode, req.body.countryCode);
+
+    if (geonamesResponse.postalCodes) {
+        res.send(geonamesResponse.postalCodes);
+    }
+        
+
+});
+
+async function getGeonamesData (zipCode, countryCode) {
+    try {
+        const url = `http://api.geonames.org/postalCodeSearchJSON?postalcode=${zipCode}&country=${countryCode}&username=${process.env.GEONMAMES_USERNAME}`;
+        const response = await fetch(url);
+        
+        return await response.json();
+    
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+}
+
+app.post("/geodata", async (req, res) => {
+    console.log(req.body);
+    const response = await getWeatherbitData(req.body.lat, req.body.lng);
+    console.log(response);
+});
+
+async function getWeatherbitData (lat, lng) {
+    try {
+        const url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}`;
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+
+
+}
