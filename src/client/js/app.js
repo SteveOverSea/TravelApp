fd9f2512354bae0e2162bd24ba06b56ebd45f499
Stefan Rausch <rausch.stefan@edumail.at>
2021-04-29 00:22:04 +0200

    delete todo comments

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,10 +2,6 @@ import { setMinMaxDates } from "./formChecker.js"
 import { submitInputForm } from "./apiRequestHandler.js"
 import { locationInputHandler } from "./locationSuggester.js"
 
-// TODO: 
-// edit readme - choose which expand project i chose
-// make API keys visible for submission
-
 // add eventhandler for form-submit, the location-input and sets min-max dates for date-input
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("submit-form").addEventListener("submit", submitInputForm);
